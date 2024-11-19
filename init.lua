ed60f24a8b6ecc677ec11a35e6362de6c2bdada0
Rémino Rem <29999+remino@users.noreply.github.com>
2024-11-19 23:24:16 +0900

    Show Nvdash on start

--- init.lua
+++ init.lua
@@ -35,3 +35,12 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    callback = function()
+        if vim.fn.argc() == 0 and vim.fn.line2byte('$') == -1 then
+            vim.cmd("Nvdash")
+        end
+    end,
+})
