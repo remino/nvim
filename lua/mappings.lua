1505eb159735daf7544d9ad63c5feb11046ada8a
Rémino Rem <29999+remino@users.noreply.github.com>
2024-11-19 23:28:29 +0900

    Map keys to NvimTree & Telescope commands

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>tt", ":Telescope<CR>", { desc = "Open Telescope" })
+map("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle Tree" })
+map("i", "<C-b>", "<esc>:NvimTreeToggle<CR>", { desc = "Toggle Tree" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
