ade56ff1f0196ee6a4bfad929bb89a3c573bdbc9
Rémino Rem <29999+remino@users.noreply.github.com>
2024-11-19 23:28:13 +0900

    Enable conform plugin, format on save

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
+    lazy = false,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
