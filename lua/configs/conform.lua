c7f774240d96bb3ba7cb39861d4dbea732e1c4b6
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-01 14:01:54 +0900

    Refactor conform.nvim config, add commands

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,57 @@
-local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format { async = true, lsp_format = "fallback", range = range }
+end, { range = true })
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
+return {
+	formatters_by_ft = {
+		css = { "prettier" },
+		graphql = { "prettier" },
+		html = { "prettier" },
+		javascript = { "prettier" },
+		javascriptreact = { "prettier" },
+		json = { "prettier" },
+		lua = { "stylua" },
+		markdown = { "prettier" },
+		python = { "isort", "black" },
+		sh = { "shfmt" },
+		svelte = { "prettier" },
+		typescript = { "prettier" },
+		typescriptreact = { "prettier" },
+		yaml = { "prettier" },
+	},
+
+	format_on_save = function(bufnr)
+		-- print debug
+		print(vim.g.disable_autoformat)
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 20000, lsp_format = "fallback" }
+	end,
 }
-
-return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,75 +1,127 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-    lazy = false,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo", "Format", "FormatDisable", "FormatEnable" },
+		keys = {
+			{
+				"<leader>cd",
+				"<Cmd>FormatDisable<CR>",
+				mode = "",
+				desc = "Disable autoformat on save",
+			},
+			{
+				"<leader>ce",
+				"<Cmd>FormatEnable<CR>",
+				mode = "",
+				desc = "Enable autoformat on save",
+			},
+			{
+				"<leader>cf",
+				"<Cmd>Format<CR>",
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = function()
+			return require "configs.conform"
+		end,
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-    lazy = false,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require "configs.lspconfig"
+		end,
+		lazy = false,
+	},
 
-  {
-    "github/copilot.vim",
-    lazy = false,
-  },
+	{
+		"github/copilot.vim",
+		lazy = false,
+	},
 
-  {
-    "brenton-leighton/multiple-cursors.nvim",
-    lazy = false,
-    version = "*", -- Use the latest tagged version
-    opts = {}, -- This causes the plugin setup function to be called
-    keys = {
-      -- <C-j> & <C-k> just won't work here. Don't know why. Added them to mappings.lua instead.
-      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
-      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+	{
+		"brenton-leighton/multiple-cursors.nvim",
+		lazy = false,
+		version = "*", -- Use the latest tagged version
+		opts = {}, -- This causes the plugin setup function to be called
+		keys = {
+			-- <C-j> & <C-k> just won't work here. Don't know why. Added them to mappings.lua instead.
+			{
+				"<C-j>",
+				"<Cmd>MultipleCursorsAddDown<CR>",
+				mode = { "n", "x" },
+				desc = "Add cursor and move down",
+			},
+			{
+				"<C-k>",
+				"<Cmd>MultipleCursorsAddUp<CR>",
+				mode = { "n", "x" },
+				desc = "Add cursor and move up",
+			},
 
-      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+			{
+				"<C-Up>",
+				"<Cmd>MultipleCursorsAddUp<CR>",
+				mode = { "n", "i", "x" },
+				desc = "Add cursor and move up",
+			},
+			{
+				"<C-Down>",
+				"<Cmd>MultipleCursorsAddDown<CR>",
+				mode = { "n", "i", "x" },
+				desc = "Add cursor and move down",
+			},
 
-      { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or remove cursor" },
+			{
+				"<C-LeftMouse>",
+				"<Cmd>MultipleCursorsMouseAddDelete<CR>",
+				mode = { "n", "i" },
+				desc = "Add or remove cursor",
+			},
 
-      { "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
-      {
-        "<Leader>A",
-        "<Cmd>MultipleCursorsAddMatchesV<CR>",
-        mode = { "n", "x" },
-        desc = "Add cursors to cword in previous area",
-      },
+			{
+				"<Leader>a",
+				"<Cmd>MultipleCursorsAddMatches<CR>",
+				mode = { "n", "x" },
+				desc = "Add cursors to cword",
+			},
+			{
+				"<Leader>A",
+				"<Cmd>MultipleCursorsAddMatchesV<CR>",
+				mode = { "n", "x" },
+				desc = "Add cursors to cword in previous area",
+			},
 
-      {
-        "<Leader>d",
-        "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
-        mode = { "n", "x" },
-        desc = "Add cursor and jump to next cword",
-      },
-      { "<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+			{
+				"<Leader>d",
+				"<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+				mode = { "n", "x" },
+				desc = "Add cursor and jump to next cword",
+			},
+			{ "<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
 
-      { "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
-    },
-  },
+			{ "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
+		},
+	},
 
-  {
-    "numToStr/Comment.nvim",
-    lazy = false,
-    opts = {
-      -- add any options here
-    },
-  },
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+		opts = {
+			-- add any options here
+		},
+	},
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
