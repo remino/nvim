6db84cf5c91a805d17cea47eb43bc2397b94fe04
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-01 18:02:44 +0900

    Format init.lua

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,14 +15,14 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
+	{
+		"NvChad/NvChad",
+		lazy = false,
+		branch = "v2.5",
+		import = "nvchad.plugins",
+	},
 
-  { import = "plugins" },
+	{ import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -33,14 +33,14 @@ require "options"
 require "nvchad.autocmds"
 
 vim.schedule(function()
-  require "mappings"
+	require "mappings"
 end)
 
 vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = "*",
-    callback = function()
-        if vim.fn.argc() == 0 and vim.fn.line2byte('$') == -1 then
-            vim.cmd("Nvdash")
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		if vim.fn.argc() == 0 and vim.fn.line2byte "$" == -1 then
+			vim.cmd "Nvdash"
+		end
+	end,
 })
