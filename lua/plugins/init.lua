522d93310b20a2e899715dccc87479affd546a3e
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-18 18:43:34 +0900

    Add markdown-toc plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -143,4 +143,12 @@ return {
 			-- })
 		end,
 	},
+	{
+		"hedyhli/markdown-toc.nvim",
+		ft = "markdown", -- Lazy load on markdown filetype
+		cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+		opts = {
+			-- Your configuration here (optional)
+		},
+	},
 }
