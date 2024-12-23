a19f9be75054776c7f8ba723a298f985c8e5ff1b
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-23 21:54:02 +0900

    Add highlighter for Hurl files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -151,4 +151,26 @@ return {
 			-- Your configuration here (optional)
 		},
 	},
+	{
+		"samueljoli/hurl.nvim",
+		lazy = false,
+		config = function()
+			require("hurl").setup {
+				comment = "#ebc021", -- default => Comment
+				method = "#fffc58", -- default => Statement
+				url = "#fffc58", -- default => Underlined
+				version = "#032ea7", -- default => Statement
+				status = "#032ea7", -- default => Number
+				section = "#032ea7", -- default => Statement
+				operators = "#c592ff", -- default => Operator
+				string = "#032ea7", -- default => String
+				query = "#d57bff", -- default => Identifier
+				filter = "#032ea7", -- default => Operator
+				predicate = "#032ea7", -- default => Operator
+				template = "#032ea7", -- default => Identifier
+				escapeQuote = "#032ea7", -- default => SpecialChar
+				escapeNumberSign = "#032ea7", -- default => SpecialChar
+			}
+		end,
+	},
 }
