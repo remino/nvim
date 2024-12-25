ce441a04c38ea7794de985b6ecb9f1da99945d92
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-25 10:54:52 +0900

    Add tailwindcss to lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "emmet_language_server" }
+local servers = { "html", "cssls", "emmet_language_server", "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
