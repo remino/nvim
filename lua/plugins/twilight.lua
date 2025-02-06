58e30f0fd2d13962e664351a32f023a21185712c
Rémino Rem <29999+remino@users.noreply.github.com>
2025-02-06 18:14:36 +0900

    Add twilight

---
+++ lua/plugins/twilight.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"folke/twilight.nvim",
+		cmd = { "Twilight", "TwilightEnable", "TwilightDisable" },
+		keys = {
+			{
+				"<leader>tw",
+				"<Cmd>Twilight<CR>",
+				mode = "",
+				desc = "Toggle Twilight",
+			},
+		},
+	},
+}
