8f06249a57528b2a102e144bc53fcb0590fa84ff
Rémino Rem <29999+remino@users.noreply.github.com>
2025-08-09 11:35:00 +0900

    Refactor copilot setup, bind Ctrl+: to accept

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,20 @@
-vim.api.nvim_create_user_command("CopilotToggle", function()
-	require("copilot.suggestion").toggle_auto_trigger()
-end, { nargs = "*" })
-
 return {
-	-- {
-	-- 	"github/copilot.vim",
-	-- 	lazy = false,
-	-- },
 	{
 		"zbirenbaum/copilot.lua",
 		cmd = { "Copilot", "CopilotToggle" },
 		event = "InsertEnter",
 		config = function()
-			require("copilot").setup {}
+			vim.api.nvim_create_user_command("CopilotToggle", function()
+				require("copilot.suggestion").toggle_auto_trigger()
+			end, { nargs = "*" })
+
+			require("copilot").setup {
+				suggestion = {
+					keymap = {
+						accept = "<C-:>",
+					},
+				},
+			}
 		end,
 		keys = {
 			{
