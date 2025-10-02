29d0a03a8b958cabdd680db5c7e3eb1fa43fa259
Rémino Rem <29999+remino@users.noreply.github.com>
2025-10-02 19:29:31 +0900

    Specify CodeCompanion commands for lazy load

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"olimorris/codecompanion.nvim",
+		cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat", "CodeCompanionCmd" },
 		opts = {},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
