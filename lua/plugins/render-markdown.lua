b7c56afef6384cb012e434e89e0ab55d8bfcd396
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-19 10:09:08 +0900

    Adjust code block background

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,6 +2,15 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+		config = function(_, opts)
+			local colors = require("base46").get_theme_tb "base_30"
+
+			vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = colors.one_bg })
+			vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorder", { fg = colors.line, bg = colors.one_bg })
+			vim.api.nvim_set_hl(0, "RenderMarkdownCodeInline", { bg = colors.one_bg })
+
+			require("render-markdown").setup(opts)
+		end,
 		opts = {
 			code = {
 				border = "thin",
