2399595fe5bc8912995e26bdf3535d6eb107ec69
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-19 15:34:10 +0900

    Enable nvim-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"kylechui/nvim-surround",
 		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
+		lazy = false,
 		config = function()
 			require("nvim-surround").setup {
 				-- Configuration here, or leave empty to use defaults
