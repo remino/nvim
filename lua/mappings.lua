f5d509b0f6b36e1f2c2a4d91605032a6bc9089c5
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-19 11:57:10 +0900

    Map <leader>ca to enabled code actions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,12 +3,20 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local function code_actions_without_disabled()
+	vim.lsp.buf.code_action {
+		filter = function(action)
+			return action.disabled == nil
+		end,
+	}
+end
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>tt", ":Telescope<CR>", { desc = "Open Telescope" })
 map("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle Tree" })
 map("i", "<C-b>", "<esc>:NvimTreeToggle<CR>", { desc = "Toggle Tree" })
+map({ "n", "v" }, "<leader>ca", code_actions_without_disabled, { desc = "Code actions" })
 map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
